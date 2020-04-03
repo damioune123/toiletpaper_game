3b11079ad27b6e30560a3c94044d91a48beb232f
Damien Meur <damienmeur@gmail.com>
2020-04-03 17:15:11 +0200

    add post route endpoint

--- api/app.js
+++ api/app.js
@@ -1,8 +1,7 @@
-// Import the Express module
+// Import the vendor modules
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-// Import the 'path' module (packaged with Node.js)
 const path = require('path');
 
 // Create a new instance of Express and socket io
@@ -16,17 +15,19 @@ app.use(bodyParser.json());
 
 // Serve static html, js, css, and image files from the built frontend 'dist'
 app.use(express.static(path.normalize(path.join(__dirname,'../gui/dist'))));
+
 //add routes for REST API
 app.use('/api', require('./routes'));
 
-io.set('log level',1);
+io.set('log level', 1);
+
 //Connect the websocket
 io.on('connection', (socket)=>{
     //TODO
     console.log('a user connected');
 });
-// Create a Node.js based http server on port 8080
 
+// Create a Node.js based http server on port 8080
 http.listen(8080, ()=>{
     console.log('listening on *:8080');
 });
